4e4c15007f9a5c2680c14a5c34054e96f448d627
TheUltDev <2079305+TheUltDev@users.noreply.github.com>
2025-07-16 13:31:07 -0500

    update hashes

--- 6.9-wasm-aqt-mt/get_emsdk.sh
+++ 6.9-wasm-aqt-mt/get_emsdk.sh
@@ -3,7 +3,7 @@
 
 [ "$SDK_VERSION" ] || SDK_VERSION=3.1.70
 [ "$SDK_URL" ]    || SDK_URL="https://github.com/emscripten-core/emsdk/archive/refs/tags/$SDK_VERSION.tar.gz"
-[ "$SDK_SHA256" ] || SDK_SHA256=6df4b778f52e0e86c721cc6f97e7ad8fd6c842be2451e39f2bc528facb5d4cde
+[ "$SDK_SHA256" ] || SDK_SHA256=5aa3fcdfbc3812419e9c73a5147ca9f9fdda897e05f24161813df14b35fcd7a5
 
 [ "$SDK_PATH" ] || SDK_PATH=/opt/emsdk
 
